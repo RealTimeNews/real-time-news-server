4dca8574d29574c01838e1bb9e6d4fe0e9f10c01
nupdoong <pjw930731@naver.com>
2022-01-19 20:13:24 +0900

    [feat] Spring Boot Project Upload

---
+++ RealTimeNews/src/test/java/com/example/realtimenews/RealTimeNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
