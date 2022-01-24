da0e2ecf4617647db4948e24818b8959b1315cb9
nupdoong <pjw930731@naver.com>
2022-01-24 13:23:43 +0900

    [feat] SignUpRequestDto 작성

--- RealTimeNews/src/main/java/com/example/realtimenews/auth/dto/SignUpRequestDto.java
+++ RealTimeNews/src/main/java/com/example/realtimenews/auth/dto/SignUpRequestDto.java
@@ -1,4 +1,21 @@
 package com.example.realtimenews.auth.dto;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequestDto {
+	@NotBlank
+	private String userId;
+	@NotBlank
+	private String password;
+	@NotBlank
+	private String userName;
 }
