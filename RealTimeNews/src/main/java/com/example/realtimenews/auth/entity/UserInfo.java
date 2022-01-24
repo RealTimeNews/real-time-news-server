0671363f354bd3b85a11168ef4829fc5b2da4e83
nupdoong <pjw930731@naver.com>
2022-01-24 13:05:35 +0900

    [feat] UserInfo Entity 작성

--- RealTimeNews/src/main/java/com/example/realtimenews/auth/entity/UserInfo.java
+++ RealTimeNews/src/main/java/com/example/realtimenews/auth/entity/UserInfo.java
@@ -1,4 +1,37 @@
 package com.example.realtimenews.auth.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Userinfo")
+@Getter
+@Setter
 public class UserInfo {
+	@Id
+	@GeneratedValue
+	private int seq;
+
+	@Column(unique = true)
+	@NotBlank
+	private String userId;
+	@NotBlank
+	private String password;
+	@NotBlank
+	private String username;
+
+	public UserInfo(){
+	}
+
+	@Builder
+	public UserInfo(@NotBlank String userId, @NotBlank String password, @NotBlank String username){
+		this.userId = userId;
+		this.password = password;
+		this.username = username;
+	}
+
 }
