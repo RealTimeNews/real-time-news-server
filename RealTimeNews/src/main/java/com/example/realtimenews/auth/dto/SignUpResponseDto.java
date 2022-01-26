f298e3fae1097a20c6458f049ec3e87b0baeedbc
nupdoong <pjw930731@naver.com>
2022-01-26 18:00:19 +0900

    [fix] Status 드랍

--- RealTimeNews/src/main/java/com/example/realtimenews/auth/dto/SignUpResponseDto.java
+++ RealTimeNews/src/main/java/com/example/realtimenews/auth/dto/SignUpResponseDto.java
@@ -2,13 +2,12 @@ package com.example.realtimenews.auth.dto;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class SignUpResponseDto {
-
-	@NotBlank
-	private String status;
 
 	@NotBlank
 	private String code;
@@ -16,8 +15,7 @@ public class SignUpResponseDto {
 	@NotBlank
 	private String message;
 
-	public SignUpResponseDto(String status, String code, String message){
-		this.status = status;
+	public SignUpResponseDto(String code, String message){
 		this.code = code;
 		this.message = message;
 	}
